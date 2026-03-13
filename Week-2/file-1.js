3033a293cca7160a0576143835e76d48deecf15a
Fawaz Salimanu <salimanufawaz@gmail.com>
2026-03-13 14:25:56 +0100

    Commited file-1.js during class

---
+++ Week-2/file-1.js
@@ -0,0 +1,21 @@
+const song = `Twinkle twinkle little star
+How I wonder what you are 
+Up above the world so high
+Like a diamond in the sky
+Twinkle twinkle little star
+How I wonder what you are`;
+
+console.log(song);
+
+const line = 'How I wonder\n\t what you are';
+console.log("\n" + line);
+
+const myName = 'Fawaz';
+const age = 18;
+
+const intro = `My name is ${myName} and I am ${age} years old.
+I was born in ${new Date().getFullYear() - age}.`;
+console.log('\n');
+console.log(intro);
+
+// String methods
